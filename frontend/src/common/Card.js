045bd0b954e6bb635e08ad699f64e5a09c4b5b3d
MohamedMuneerM <rehanamujeebh@gmail.com>
2021-04-06 15:47:20 +0530

    updated

--- frontend/src/common/Card.js
+++ frontend/src/common/Card.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import classNames from 'classnames';
 
+<<<<<<< HEAD
 const Card = ({ cardStyle = '', className, children, ...others }) => {
+=======
+const Card = ({ cardStyle = '', className = '', children, ...others }) => {
+  const cardClass = `${className && className + ' '}card ${
+    cardStyle === 'dark' && 'card--dark'
+  }`;
+
+>>>>>>> 65fc61a... :art: update feed ui styles
   return (
     <div
       className={classNames(className, 'card', {
